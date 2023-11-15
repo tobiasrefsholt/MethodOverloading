94a0b2d263532f104e356ea96ea538159f0572fd
tobias <tobias@refsholt.net>
2023-11-15 11:04:30 +0100

    Remove unused code

--- MethodOverloading/OverloadTest.cs
+++ MethodOverloading/OverloadTest.cs
@@ -2,22 +2,6 @@ namespace MethodOverloading;
 
 public class OverloadTest
 {
-    public int Number;
-    public double desimal;
-    public string SomethingSomething;
-
-    /*public OverloadTest(double desimal, string somethingSomething)
-    {
-        double Desimal = desimal;
-        string SomethingSomething = somethingSomething;
-    }*/
-    
-    /*public OverloadTest(int number, string somethingSomething)
-    {
-        Number = number;
-        SomethingSomething = somethingSomething;
-    }*/
-
     public void PrintMessage(int number, string somethingSomething)
     {
         Console.WriteLine($"Du skrev denne stringen \"{somethingSomething}\" og dette tallet \"{number}\"");
